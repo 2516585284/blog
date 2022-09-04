be580a72036a247ea9c9c888ff75c5c9f4607a59
chenli <2516585284@qq.com>
2022-09-04 21:48:50 +0800

    日志配置

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger('django')
+
+def log(request):
+
+    logger.info('info')
+    return HttpResponse('test')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', log)
 ]
