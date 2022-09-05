cecfaf31dbbfc3ae7da36020a4e481f9f397ffa7
chenli <2516585284@qq.com>
2022-09-05 15:06:44 +0800

    图片验证码实现

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ImageCodeView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode')
 ]
--- blog/users/views.py
+++ blog/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.http.response import HttpResponseBadRequest
+from libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -10,3 +14,30 @@ class RegisterView(View):
 
         return render(request, 'register.html')
 
+
+# 图片验证码试图
+class ImageCodeView(View):
+
+    def get(self, request):
+        """
+        1. 接收前端传递过来的uuid
+        2. 判断uuid是否获取到
+        3. 通过调用captcha来生成图片验证码（图片二进制和图片内容）
+        4. 将图片内容保存到redis中
+            uuid作为一个key， 图片内容作为一个value 同时还需要设置一个时效
+        5. 返回图片二进制
+        :param request:
+        :return:
+        """
+
+        uuid = request.GET.get('uuid')
+
+        if uuid is None:
+            return HttpResponseBadRequest('没有传递uuid')
+
+        text, image = captcha.generate_captcha()
+
+        redis_conn = get_redis_connection('default')
+        redis_conn.setex('img:%s'%uuid, 300, text)
+
+        return HttpResponse(image, content_type='image/jpeg')
