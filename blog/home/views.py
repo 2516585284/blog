a87f6f7c32bb1380ae1c641f491f61224be226f0
chenli <2516585284@qq.com>
2022-09-18 09:48:28 +0800

    博客首页文章数据展示功能实现

--- blog/home/views.py
+++ blog/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from home.models import ArticleCategory, Article
+from django.http.response import HttpResponseNotFound
 
 # Create your views here.
 
@@ -8,4 +10,37 @@ class IndexView(View):
 
     def get(self, request):
 
-        return render(request, 'index.html')
+        categories = ArticleCategory.objects.all()
+        cat_id = request.GET.get('cat_id', 1)
+
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseNotFound('没有此分类')
+
+        # 获取分页参数
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+
+        articles = Article.objects.filter(category=category)
+
+        from django.core.paginator import Paginator, EmptyPage
+        paginator = Paginator(articles, per_page=page_size)
+
+        try:
+            page_articles = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('empty page')
+
+        total_page = paginator.num_pages
+
+        context={
+            'categories':categories,
+            'category':category,
+            'articles':page_articles,
+            'page_num':page_num,
+            'page_size':page_size,
+            'total_page':total_page,
+        }
+
+        return render(request, 'index.html', context=context)
